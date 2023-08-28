7df42d8debda4b3a1690df2ae7388a63a266479b
Alvaro "Chamo" Linares Cabre <alvarolinarescabre@gmail.com>
2023-08-28 20:26:47 +0200

    Update persons.sql

--- mysql/persons.sql
+++ mysql/persons.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Persons (ID varchar (5)
 NOT NULL PRIMARY KEY, FirstName VARCHAR(30),
 LastName VARCHAR(30), Age INT);
-INSERT INTO Persons (ID, FirstName, LastName, Age) VALUES ("0001", "Chamo", "Linares", 49);
+INSERT INTO Persons (ID, FirstName, LastName, Age) VALUES ("0001", "Chamo", "Linares", 52);
 
